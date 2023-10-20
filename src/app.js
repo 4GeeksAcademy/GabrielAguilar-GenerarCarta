d6383383101cae059f720903e5d517e82acb0141
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-10-20 16:26:52 +0000

    generador de carta

--- src/app.js
+++ src/app.js
@@ -6,6 +6,59 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function getRandomCard() {
+    //generar aleatoriamente un suit y un numero
+
+    const randomSuitIndex = Math.floor(Math.random() * suits.length);
+    const randomSuit = suits[randomSuitIndex];
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    //link para html
+
+    document.getElementById("top-suit").innerHTML = randomSuit;
+    document.getElementById("card-number").innerHTML = randomNumber;
+    document.getElementById("bottom-suit").innerHTML = randomSuit;
+
+    //condicion para que cambie de color los items
+
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      cardContain.classList.add("red");
+    } else {
+      cardContain.classList.remove("red");
+    }
+    // respuesta
+    return randomSuit + randomSuit + randomNumber;
+  }
+  //asignacion de divs html padre a la condicion
+  const cardContain = document.getElementById("card-contain");
+  //llamar a la funcion principal
+  getRandomCard();
+
+  const refreshButton = document.getElementById("refresh-button");
+
+  refreshButton.addEventListener("click", function() {
+    getRandomCard();
+  });
+
+  //Temporizador que cambia la carta cada 10 segundos
+  setInterval(function() {
+    getRandomCard();
+  }, 10000);
 };
