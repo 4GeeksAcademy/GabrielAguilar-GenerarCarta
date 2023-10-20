c0ade2eb73f473f4bea23bfde9e47704bc35c70d
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-10-20 19:24:19 +0000

    cartas generadas

--- src/app.js
+++ src/app.js
@@ -36,29 +36,31 @@ window.onload = function() {
     document.getElementById("card-number").innerHTML = randomNumber;
     document.getElementById("bottom-suit").innerHTML = randomSuit;
 
-    //condicion para que cambie de color los items
+    const cardContain = document.getElementById("card-contain");
 
+    widthInput.addEventListener("input", function() {
+      cardContain.style.width = widthInput.value + "px";
+    });
+
+    heightInput.addEventListener("input", function() {
+      cardContain.style.height = heightInput.value + "px";
+    });
+
+    //condicion para que cambie de color los items
     if (randomSuit === "♦" || randomSuit === "♥") {
       cardContain.classList.add("red");
     } else {
       cardContain.classList.remove("red");
     }
-    // respuesta
+    // concatenar cadenas
     return randomSuit + randomSuit + randomNumber;
   }
-  //asignacion de divs html padre a la condicion
+  //selecciono el elemento HTML con el ID card-contain y lo asigna a la variable cardContain
   const cardContain = document.getElementById("card-contain");
-  //llamar a la funcion principal
   getRandomCard();
 
-  const refreshButton = document.getElementById("refresh-button");
-
-  refreshButton.addEventListener("click", function() {
-    getRandomCard();
-  });
+  letterChange.addEventListener("click", getRandomCard);
 
   //Temporizador que cambia la carta cada 10 segundos
-  setInterval(function() {
-    getRandomCard();
-  }, 10000);
+  setInterval(getRandomCard, 10000);
 };
